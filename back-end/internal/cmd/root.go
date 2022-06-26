cdcf173ed5a8f303ed05c0aa9604a05c3fe8e50b
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-26 20:25:31 +0430

    add: database connection

--- back-end/internal/cmd/root.go
+++ back-end/internal/cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 
+	"github.com/amirhnajafiz/hollyworld/back-end/internal/handler"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,11 +12,15 @@ func Execute() {
 	// creating a fiber app
 	app := fiber.New()
 
+	// creating a handler
+	h := handler.Handler{}
+
+	// defining our endpoints
+	app.Get("/api/movies", h.GetTopMovies)
+	app.Get("/api/movies/:id", h.GetSingleMovie)
+	app.Get("/dash/:id", h.GetMovieFile)
+
 	// listening on port 8080
 	log.Fatal(app.Listen(":8080"))
-	// TODO: fiber handler
 	// TODO: migrate file
-	// TODO: top 6 endpoint
-	// TODO: a single endpoint
-	// TODO: serve file
 }
