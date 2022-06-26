1a7b872d860e2e819f8f4d1a872ba94bb49465c3
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-26 20:03:43 +0430

    init: cmd

---
+++ back-end/internal/cmd/root.go
@@ -0,0 +1,10 @@
+package cmd
+
+func Execute() {
+	// TODO: start fiber server
+	// TODO: fiber handler
+	// TODO: migrate file
+	// TODO: top 6 endpoint
+	// TODO: a single endpoint
+	// TODO: serve file
+}
---
+++ back-end/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/amirhnajafiz/hollyworld/back-end/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
