55aae4fa0cd000c07359fec54f03987f08459bf7
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-26 21:03:16 +0430

    feat: get top 6 movies

--- back-end/internal/handler/handler.go
+++ back-end/internal/handler/handler.go
@@ -8,12 +8,46 @@ import (
 
 // Handler is our endpoint handling struct
 type Handler struct {
-	Db *sql.Conn
+	Db *sql.DB
 }
 
-// GetTopMovies returns 6 top movies
+// GetTopMovies returns top 6 movies
 func (h *Handler) GetTopMovies(c *fiber.Ctx) error {
-	return nil
+	// creating a movie type
+	type Movie struct {
+		Uid    int    `json:"uid"`
+		Title  string `json:"title"`
+		Poster string `json:"poster"`
+	}
+
+	// creating our variables
+	var (
+		movie  Movie
+		movies []Movie
+
+		query = "SELECT uid, title, poster FROM movies ORDER BY score LIMIT 6"
+	)
+
+	// executing database query
+	rows, err := h.Db.Query(query)
+	if err != nil {
+		return err
+	}
+
+	// scan output
+	for rows.Next() {
+		er := rows.Scan(&movie)
+		if er != nil {
+			return er
+		}
+
+		movies = append(movies, movie)
+	}
+
+	// close rows
+	_ = rows.Close()
+
+	return c.JSON(movies)
 }
 
 // GetSingleMovie returns a single movie information by id
