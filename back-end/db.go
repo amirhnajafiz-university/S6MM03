750d93c7aad984247a5c7e53ec0835ca94361686
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-26 21:13:28 +0430

    add: database migration file

---
+++ back-end/db.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/amirhnajafiz/hollyworld/back-end/internal/database"
+)
+
+func main() {
+	// creating a database connection
+	d, err := database.NewConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// making the migration
+	err = database.Migrate(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
