ba2db75b2a01a5440102de5a7cae17b14aff7a3a
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-29 23:52:15 +0430

    feat: mdp output for files

--- python/script.py
+++ python/script.py
@@ -8,13 +8,17 @@ from ffmpeg_streaming import Formats
 BASE = "tmp/dash/"
 
 # getting file name
-file_name = input()
-file_id = input()
+file_name = input("[NAME] > ")
+file_id = input("[OUTPUT ID] > ")
 
 # creating a video instance
 video = ffmpeg_streaming.input(file_name)
 
+print("Processing ...")
+
 # creating a dash video
 dash = video.dash(Formats.h264())
 dash.auto_generate_representations()
-dash.output(BASE + file_id)
+dash.output(BASE + file_id + ".mpd")
+
+print("Done.")
