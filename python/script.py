e12056e04ce16d2d07ad1d3ad0960dee9c8708f3
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-29 23:44:51 +0430

    add: python script

--- python/script.py
+++ python/script.py
@@ -1,5 +1,20 @@
 # importing ffmpeg streaming
 import ffmpeg_streaming
+from ffmpeg_streaming import Formats
 
 
 
+# const
+BASE = "tmp/dash/"
+
+# getting file name
+file_name = input()
+file_id = input()
+
+# creating a video instance
+video = ffmpeg_streaming.input(file_name)
+
+# creating a dash video
+dash = video.dash(Formats.h264())
+dash.auto_generate_representations()
+dash.output(BASE + file_id)
