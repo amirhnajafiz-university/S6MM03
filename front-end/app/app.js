ef4f9bbbe272b309c8f4ce4393789b76d826a398
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-07-02 07:47:39 +0430

    done but apis should get fixed

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,18 +1,36 @@
-'use strict';
+var app = angular.module('myApp', ['ngRoute'])
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'})
-      .when('/view1', {
-        templateUrl: 'view1/view1.html',
-        controller: 'View1Ctrl'
-      });
-}]);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home/first.html',
+            controller: 'myCtrl'
+        })
+        .when('/movies/:id', {
+            templateUrl: 'home/second.html',
+            controller: 'secondController'
+        })
+});
+
+app.controller('myCtrl', ['$scope', function ($scope) {
+
+        $scope.movies2 = JSON.parse('[{"id":1,"title":"Doctor Strange in the Multiverse of Madness"},{"id":6,"title":"The Black Phone"},{"id":2,"title":"Obi-Wan Kenobi"},{"id":5,"title":"The Boys"},{"id":3,"title":"Top Gun: Maverick"},{"id":4,"title":"Stranger Things"}]')
+        $scope.count = 0;
+        $scope.myClick = function () {
+            $scope.count++;
+        };
+    }]
+);
+
+app.controller('secondController', ['$scope', '$routeParams', function ($scope, $routeParams) {
+
+        $scope.count = 0;
+        $scope.movieInfo = JSON.parse('{"id":2,"title":"Obi-Wan Kenobi","director":"Deborah Chow","score":8,"description":"Jedi Master Obi-Wan Kenobi has to save young Leia after she is kidnapped, all the while being pursued by Imperial Inquisitors and his former Padawan, now known as Darth Vader.","type":"DASH1","link":""}')
+        $scope.posterPath = 'localhost:8080/poster/' + $routeParams.id;
+        $scope.dash = 'localhost:8080/dash/' + $routeParams.id;
+        $scope.myClick = function () {
+            $scope.count++;
+        }
+    }]
+);
--- front-end/e2e-tests/scenarios.js
+++ front-end/e2e-tests/scenarios.js
@@ -5,20 +5,20 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /home when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/home");
   });
 
 
-  describe('view1', function() {
+  describe('home', function() {
 
     beforeEach(function() {
       browser.get('index.html#!/view1');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render home when user navigates to /home', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
@@ -26,14 +26,14 @@ describe('my app', function() {
   });
 
 
-  describe('view2', function() {
+  describe('movies', function() {
 
     beforeEach(function() {
       browser.get('index.html#!/view2');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render movies when user navigates to /movies', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 2/);
     });
